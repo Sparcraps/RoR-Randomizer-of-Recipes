d0011fcb4a9041487a08a8c7d2ab78c97ad8f780
Sparcraps <jakob.vanky@live.se>
2024-02-20 14:23:52 +0100

    All basic functions added. Still missing descriptions and test cases

--- ingredients.ts
+++ ingredients.ts
@@ -2,10 +2,17 @@ import {
     Pair
 } from "./lib/list";
 
+/**
+ * Tag type for record type checking.
+ */
 type TaggedRecord = {
     tag: string
 }
 
+/**
+ * Ingredient data type; the main data type of RoR.
+ * Contains all ingredient data.
+ */
 export type Ingredient = {
     tag: "ingredient",
     name: string,
@@ -14,16 +21,24 @@ export type Ingredient = {
     history: Array<string>,
     measurement: string,
     kcal_per_measurement: number, // to determine portion size
-    range: Pair<number, number> // lower kcal limit, upper kcal limit
+    range: Pair<number, number> // (lower kcal limit, upper kcal limit)
     // nutrition_type: Array<string>, // e.g. vitamin A, protein, fat, carbs
 }
 
+/**
+ * Ingredient category data type.
+ * Contains the available cooking methods of ingredients.
+ */
 type IngredientCategory = {
     tag: "ingredientcategory",
     name: string,
     cooking_methods: Array<string>
 }
 
+/**
+ * Kitchenware data type.
+ * Has an inventory of ingredients currently contained in the kitchenware.
+ */
 type KitchenWare = {
     tag: "kitchenware",
     name: string,
@@ -31,24 +46,63 @@ type KitchenWare = {
 }
 
 
-
 function is_ingredient(input: TaggedRecord): input is Ingredient {
-    return input.tag === "ingredient";    
+    return input.tag === "ingredient";
 }
+function is_ingredient_category(input: TaggedRecord): input is IngredientCategory {
+    return input.tag === "ingredientcategory";
+}
+function is_kitchenware(input: TaggedRecord): input is KitchenWare {
+    return input.tag === "kitchenware";
+}
+
 function get_ingredient_name(ingredient: Ingredient): string {
     return ingredient.name;
 }
-function get_ingredient_calories(ingredient: Ingredient): number {
+function get_ingredient_allergies(ingredient: Ingredient): Array<string> {
+    return ingredient.allergies;
+}
+function get_ingredient_history(ingredient: Ingredient): Array<string> {
+    return ingredient.history;
+}
+function get_ingredient_measurement(ingredient: Ingredient): string {
+    return ingredient.measurement;
+}
+function get_ingredient_kcal(ingredient: Ingredient): number {
     return ingredient.kcal_per_measurement;
+}
+function get_ingredient_kcal_range(ingredient: Ingredient): Pair<number, number> {
+    return ingredient.range;
+}
+function get_category_name(ingredient: Ingredient): string {
+    return ingredient.ingredient_type.name;
 }
 function get_ingredient_cooking_methods(ingredient: Ingredient): Array<string> {
     return ingredient.ingredient_type.cooking_methods;
 }
-function get_ingredient_type(ingredient: Ingredient): IngredientCategory {
-    return ingredient.ingredient_type;
+function get_kitchenware_inventory(kitchenware: KitchenWare): Array<Ingredient> {
+    return kitchenware.inventory;
 }
-// export function get_ingredient_nutrition_type(ingredient: Ingredient): Array<string> {
-//     return ingredient.nutrition_type;
+
+function create_category(name: string): IngredientCategory {
+    return { tag: "ingredientcategory", name: name, cooking_methods: [] }
+}
+function create_kitchenware(name: string): KitchenWare {
+    return { tag: "kitchenware", name: name, inventory: [] }
+}
+
+function add_to_ingredient_history(ingredient: Ingredient, str: string): Ingredient {
+    get_ingredient_history(ingredient).push(str);
+    return ingredient;
+}
+function add_ingredient_to_kitchenware(ingredient: Ingredient, kitchenware: KitchenWare): KitchenWare {
+    kitchenware.inventory.push(ingredient);
+    return kitchenware;
+}
+// Not sure if this is going to be used:
+// function add_cooking_method_to_ingredient(ingredient: Ingredient, cooking_method: string): Ingredient {
+//     ingredient.ingredient_type.cooking_methods.push(cooking_method);
+//     return ingredient;
 // }
 
 function is_vegetarian(ingredient: Ingredient): boolean {
