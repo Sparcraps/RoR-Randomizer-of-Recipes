0fb97be1668b80bc30d14a262c6af7c30e657c84
isa1559 <145349249+isa1559@users.noreply.github.com>
2024-02-26 16:32:10 +0100

    export function filter allergies

--- filter.ts
+++ filter.ts
@@ -4,7 +4,7 @@ import {
 
 
 
-function filter_allergies(ingredients: Array<Array<Ingredient>>, allergies: Array<string>): Array<Array<Ingredient>>{
+export function filter_allergies(ingredients: Array<Array<Ingredient>>, allergies: Array<string>): Array<Array<Ingredient>>{
     for(let categoryindex = 0; categoryindex < ingredients.length; categoryindex ++)
     {
         for(let ingredientindex = ingredients[categoryindex].length - 1; ingredientindex >= 0; ingredientindex = ingredientindex - 1)
