c7c68f3293ec2423326fbbc9d0c7d7abd0bcb304
Sparcraps <jakob.vanky@live.se>
2024-02-27 12:10:52 +0100

    old .js file removed and function call added to input loop

--- input_loop.ts
+++ input_loop.ts
@@ -70,7 +70,7 @@ function recipimize(): void {
     if (user_input === "r") {
         return;
     } else if (user_input === "s") {
-        // save_recipe(recipe);
+        save_new_recipe(recipe);
     } else if (user_input === "b") {
         oblivion();
     }
