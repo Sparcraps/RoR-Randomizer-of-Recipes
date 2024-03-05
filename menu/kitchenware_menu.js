16b05a352caf9d8259c3d3acf72a53c7226e7a70
Sparcraps <jakob.vanky@live.se>
2024-03-05 17:19:55 +0100

    old .js leftover files removed

--- menu/kitchenware_menu.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.configure_kitchenware = void 0;
-function configure_kitchenware() {
-}
-exports.configure_kitchenware = configure_kitchenware;
