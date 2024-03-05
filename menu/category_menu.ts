99a8a446dcf8a15ddbbe84dc6872d95ca3ccb80e
Sparcraps <jakob.vanky@live.se>
2024-03-05 10:36:22 +0100

    category_menu.ts created

---
+++ menu/category_menu.ts
@@ -0,0 +1,11 @@
+import {
+    prompt
+} from "../RoR";
+
+/**
+ * A submenu of the configurations menu, where the user can configure
+ * the categories used for recipe generation, by adding or removing them.
+ */
+export function configure_category(): void {
+    
+}
